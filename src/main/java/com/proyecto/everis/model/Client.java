3f0a1e8d7eda2d99a4556979e88cf91fe52164cb
dfernava <67983816+dfernava@users.noreply.github.com>
2020-07-16 18:28:03 -0500

    16-07

--- src/main/java/com/proyecto/everis/model/Client.java
+++ src/main/java/com/proyecto/everis/model/Client.java
@@ -1,63 +1,18 @@
 package com.proyecto.everis.model;
 
-import javax.persistence.Id;
-
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
 @Document(collection = "client")
 public class Client {
 	
-	@Id
 	private String id;
-	
 	private String typeDoc;
-	
 	private String typeClient;
-	
-	private String numDoc;
-	
+	private String numDoc;	
 	private String nameClient;
-
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
-	public String getTypeDoc() {
-		return typeDoc;
-	}
-
-	public void setTypeDoc(String typeDoc) {
-		this.typeDoc = typeDoc;
-	}
-
-	public String getTypeClient() {
-		return typeClient;
-	}
-
-	public void setTypeClient(String typeClient) {
-		this.typeClient = typeClient;
-	}
-
-	public String getNumDoc() {
-		return numDoc;
-	}
-
-	public void setNumDoc(String numDoc) {
-		this.numDoc = numDoc;
-	}
-
-	public String getNameClient() {
-		return nameClient;
-	}
-
-	public void setNameClient(String nameClient) {
-		this.nameClient = nameClient;
-	}
-	
-	
 
 }
